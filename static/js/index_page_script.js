73c4fcf7824f02a3c61069f1c8d8567ed9a0aff4
Re_Gelu <morandva@yandex.ru>
2022-11-29 07:41:19 +0300

    Usernames fix

--- static/js/index_page_script.js
+++ static/js/index_page_script.js
@@ -10,15 +10,38 @@ document.querySelector('#room-name-submit').onclick = function(e) {
     const roomName = document.querySelector('#room-name-input').value;
     if (roomName) {
         const username = document.querySelector('#user-name-input').value;
-        if (username) {
-            localStorage.setItem('username', username);
+        if (localStorage.getItem('username')) {
             window.location.pathname = '/chat/' + roomName + '/';
         }
         else {
-            alert("Input username please!");
+            if (username) {
+                localStorage.setItem('username', username);
+                window.location.pathname = '/chat/' + roomName + '/';
+            }
+            else {
+                alert("Input username please!");
+            }
         }
     }
     else {
         alert("Input room name please!");
     }
 };
+
+document.querySelectorAll('#room-connect-submit').forEach(function(element) {
+    element.onclick = function(e) {
+        const username = document.querySelector('#user-name-input').value;
+        if (localStorage.getItem('username')) {
+            window.location.pathname = element.getAttribute("link");
+        } 
+        else {
+            if (username) {
+                localStorage.setItem('username', username);
+                window.location.pathname = element.getAttribute("link");
+            }
+            else {
+                alert("Input username please!");
+            }
+        }
+    };
+});
